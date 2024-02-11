18e09509a45de1d78095f9530fbe2080ce107c92
sofiakwok <zazakwok@gmail.com>
2024-02-10 20:48:08 -0500

    adding second pair of servo offsets

--- tangent_function.m
+++ tangent_function.m
@@ -45,7 +45,7 @@ d = 3.052717; % steering length
 l = 0.56; %servo arm length
 r = 0.483608; %rudder length
 
-m_1 = -0.242625;%437008;
+m_1 = -0.704724;
 m_2 = 0.405512;
 
 theta = linspace(0, 3.1415/2, 100); %all trig in radians
@@ -72,7 +72,7 @@ beta_2 = 2*(atan(top_2./bottom));
 deg_beta = beta * 180 / 3.1415;
 deg_beta_2 = beta_2 * 180 / 3.1415;
 
-figure(3);
+figure(2);
 hold on
 plot(theta*180/3.1415, deg_beta)
 plot(theta*180/3.1415, deg_beta_2)
@@ -87,7 +87,7 @@ d = 3.052717; % steering length
 l = 0.56; %servo arm length
 r = 0.483608; %rudder length
 
-m_1 = 0.242520;
+m_1 = 0.704724;
 m_2 = 0.405512; %2
 
 theta = linspace(0, 3.1415/2, 100); %all trig in radians
@@ -96,6 +96,7 @@ x_1 = -f*sin(theta);
 y_1 = -f*cos(theta);
 
 % calculate beta for alpha to stay at 0 throughout cycle
+alpha = 0/180*3.1415;
 x_2 = -r* sin(theta + alpha) + x_1;
 y_2 = -r * cos(theta + alpha) + y_1; 
 % x_2 = (l + f) * sin(theta);
@@ -107,7 +108,91 @@ c = d^2 - l^2 - 2*l*m_2 + 2*l*y_2 - m_1^2 + 2*m_1*x_2 - m_2^2 + 2*m_2*y_2 - x_2.
 top = real(0.5*sqrt(a - 4.*b.*c)) - 2*l*m_1 + 2*l.*x_2; 
 bottom = d^2 - l^2 + 2*l*m_2 - 2*l*y_2 - m_1^2 + 2*m_1*x_2 - m_2^2 + 2*m_2*y_2 - x_2.^2 - y_2.^2;
 beta = 2*(atan(top./bottom));
+top_2 = -0.5*sqrt(a - 4.*b.*c) - 2*l*m_1 + 2*l.*x_2;
+beta_2 = 2*(atan(top_2./bottom));
+
+deg_beta = beta * 180 / 3.1415;
+deg_beta_2 = beta_2 * 180 / 3.1415;
+
+figure(3);
+hold on
+plot(theta*180/3.1415, deg_beta)
+plot(theta*180/3.1415, deg_beta_2)
+xlabel('\theta')
+ylabel('\beta')
+
+%% fin1_2 beta
+%graphing beta over the feasible range that x1, y1 can be at
+
+f = 2.15178; % fin length
+d = 2.9; % steering length
+l = 0.56; %servo arm length
+r = 0.52; %rudder length
+
+m_1 = -0.185039;
+m_2 = 0.405512;
+
+theta = linspace(0, 3.1415/2, 100); %all trig in radians
+
+x_1 = f*sin(theta);
+y_1 = -f*cos(theta);
+
+% calculate beta for alpha to stay at 0 throughout cycle
+alpha = 0/180*3.1415;
+x_2 = r*sin(theta + alpha) + x_1;
+y_2 = -r*cos(theta + alpha) + y_1;
+% x_2 = (l + f) * sin(theta);
+% y_2 = -(l + f) * cos(theta);
+
+a = (4*l*x_2 - 4*l*m_1).^2;
+b = d^2 - l^2 + 2*l*m_2 - 2*l*y_2 - m_1^2 + 2*m_1*x_2 - m_2^2 + 2*m_2*y_2 - x_2.^2 - y_2.^2;
+c = d^2 - l^2 - 2*l*m_2 + 2*l*y_2 - m_1^2 + 2*m_1*x_2 - m_2^2 + 2*m_2*y_2 - x_2.^2 - y_2.^2;
+top = 0.5*sqrt(a - 4.*b.*c) + 2*l*m_1 - 2*l.*x_2; 
+bottom = d^2 - l^2 + 2*l*m_2 - 2*l*y_2 - m_1^2 + 2*m_1*x_2 - m_2^2 + 2*m_2*y_2 - x_2.^2 - y_2.^2;
+beta = 2*(atan(top./bottom));
 top_2 = -0.5*sqrt(a - 4.*b.*c) + 2*l*m_1 - 2*l.*x_2;
+beta_2 = 2*(atan(top_2./bottom));
+
+deg_beta = beta * 180 / 3.1415;
+deg_beta_2 = beta_2 * 180 / 3.1415;
+
+figure(2);
+hold on
+plot(theta*180/3.1415, deg_beta)
+plot(theta*180/3.1415, deg_beta_2)
+xlabel('\theta')
+ylabel('\beta')
+
+%% fin2_2 beta
+%graphing beta over the feasible range that x1, y1 can be at
+
+f = 2.15178; % fin length
+d = 2.85; % steering length
+l = 0.56; %servo arm length
+r = 0.52; %rudder length
+
+m_1 = 0.185039;
+m_2 = 0.405512; %2
+
+theta = linspace(0, 3.1415/2, 100); %all trig in radians
+
+x_1 = -f*sin(theta);
+y_1 = -f*cos(theta);
+
+% calculate beta for alpha to stay at 0 throughout cycle
+alpha = 0/180*3.1415;
+x_2 = -r* sin(theta + alpha) + x_1;
+y_2 = -r * cos(theta + alpha) + y_1; 
+% x_2 = (l + f) * sin(theta);
+% y_2 = -(l + f) * cos(theta);
+
+a = (4*l*m_1 - 4*l*x_2).^2;
+b = d^2 - l^2 + 2*l*m_2 - 2*l*y_2 - m_1^2 + 2*m_1*x_2 - m_2^2 + 2*m_2*y_2 - x_2.^2 - y_2.^2;
+c = d^2 - l^2 - 2*l*m_2 + 2*l*y_2 - m_1^2 + 2*m_1*x_2 - m_2^2 + 2*m_2*y_2 - x_2.^2 - y_2.^2;
+top = real(0.5*sqrt(a - 4.*b.*c)) - 2*l*m_1 + 2*l.*x_2; 
+bottom = d^2 - l^2 + 2*l*m_2 - 2*l*y_2 - m_1^2 + 2*m_1*x_2 - m_2^2 + 2*m_2*y_2 - x_2.^2 - y_2.^2;
+beta = 2*(atan(top./bottom));
+top_2 = -0.5*sqrt(a - 4.*b.*c) - 2*l*m_1 + 2*l.*x_2;
 beta_2 = 2*(atan(top_2./bottom));
 
 deg_beta = beta * 180 / 3.1415;
