efca944f324faa2bff176e01af4e44fc312d7ad4
sofiakwok <zazakwok@gmail.com>
2024-04-16 10:04:31 -0400

    creating class for apriltag

---
+++ opencv/apriltag_class.py
@@ -0,0 +1,66 @@
+# https://pyframesearch.com/2020/11/02/apriltag-with-python/
+# import the necessary packages
+import pupil_apriltags
+import argparse
+import cv2
+
+class AprilTag():
+    def __init__(self):
+        # construct the argument parser and parse the arguments
+        ap = argparse.ArgumentParser()
+        ap.add_argument("-i", "--frame", required=True,
+            help="path to input frame containing AprilTag")
+        args = vars(ap.parse_args())
+
+        self.cap = cv2.VideoCapture(0)
+        if not self.cap.isOpened():
+            print("Cannot open camera")
+            exit()
+    def get_position(self):
+        # Capture frame-by-frame
+        ret, frame = self.cap.read()
+        # if frame is read correctly ret is True
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+        # Our operations on the frame come here
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # define the AprilTags detector options and then detect the AprilTags
+        # in the input frame
+        print("[INFO] detecting AprilTags...")
+        options = pupil_apriltags.DetectorOptions(families="tag36h11")
+        detector = pupil_apriltags.Detector(options)
+        results = detector.detect(gray)
+        print("[INFO] {} total AprilTags detected".format(len(results)))
+        # Display the resulting frame
+        # loop over the AprilTag detection results
+        for r in results:
+            # extract the bounding box (x, y)-coordinates for the AprilTag
+            # and convert each of the (x, y)-coordinate pairs to integers
+            (ptA, ptB, ptC, ptD) = r.corners
+            ptB = (int(ptB[0]), int(ptB[1]))
+            ptC = (int(ptC[0]), int(ptC[1]))
+            ptD = (int(ptD[0]), int(ptD[1]))
+            ptA = (int(ptA[0]), int(ptA[1]))
+            # draw the bounding box of the AprilTag detection
+            cv2.line(frame, ptA, ptB, (0, 255, 0), 2)
+            cv2.line(frame, ptB, ptC, (0, 255, 0), 2)
+            cv2.line(frame, ptC, ptD, (0, 255, 0), 2)
+            cv2.line(frame, ptD, ptA, (0, 255, 0), 2)
+            # draw the center (x, y)-coordinates of the AprilTag
+            (cX, cY) = (int(r.center[0]), int(r.center[1]))
+            cv2.circle(frame, (cX, cY), 5, (0, 0, 255), -1)
+            # draw the tag family on the frame
+            tagFamily = r.tag_family.decode("utf-8")
+            cv2.putText(frame, tagFamily, (ptA[0], ptA[1] - 15),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+            print("[INFO] tag family: {}".format(tagFamily))
+        # show the output frame after AprilTag detection
+        cv2.imshow("frame", frame)
+        cv2.waitKey(0)
+
+    def stop(self):
+        # When everything done, release the capture
+        self.cap.release()
+        cv2.destroyAllWindows()
+
